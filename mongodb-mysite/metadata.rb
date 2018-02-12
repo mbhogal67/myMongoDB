0dba4ac6d38662f89d411114244d613ea8cc5f4a
mbhogal67 <36393345+mbhogal67@users.noreply.github.com>
2018-02-12 00:03:58 -0500

    Create metadata.rb

---
+++ mongodb-mysite/metadata.rb
@@ -0,0 +1,6 @@
+name          "mongodb-mysite"
+description   'cookbok for mongodb Berksfile install'
+maintainer    "mbtest"
+license       "Apache 2.0"
+version       "1.0.0"
+depends       'mongodb'
